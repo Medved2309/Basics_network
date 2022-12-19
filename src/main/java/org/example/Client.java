0405883925249dba2d25e7ee1968375b115b47ac
Andrey Dmitrievich <medved2309@gmail.com>
2022-12-19 14:58:13 +0500

    zadacha 1

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        try (Socket Socket = new Socket(ServerConfig.HOST, ServerConfig.PORT);
+             PrintWriter out = new PrintWriter(Socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(Socket.getInputStream()))) {
+            out.println("Алексей");
+            System.out.println(in.readLine());
+
+
+
+        }
+    }
+
+
+
+
+
+
+}
